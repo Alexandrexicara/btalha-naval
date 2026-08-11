7d7cbd760e7d27db8ba4ad223f0a1a18f5d67ed2
Alexandrexicara <santossilvacelio73@gmail.com>
2026-08-11 18:32:20 -0300

    Servidor corrigido e rodando

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORTA = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/')));
 
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
-  console.log('✅ Servidor rodando em http://localhost:' + PORTA);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log('✅ Servidor rodando em http://localhost:' + PORT);
 });
